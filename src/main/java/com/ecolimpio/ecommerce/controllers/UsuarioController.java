4c84182dcb993358807b626a700218df3e6ffaa1
Matías Rojas <matiasemanuelrojas0112@gmail.com>
2025-09-23 16:37:08 -0300

    agregado método para buscar usuario por email

--- src/main/java/com/ecolimpio/ecommerce/controllers/UsuarioController.java
+++ src/main/java/com/ecolimpio/ecommerce/controllers/UsuarioController.java
@@ -1,5 +1,9 @@
 package com.ecolimpio.ecommerce.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,14 @@ import com.ecolimpio.ecommerce.services.UsuarioService;
 public class UsuarioController extends BaseController<Usuario, String> {
     public UsuarioController(UsuarioService usuarioService) {
         super(usuarioService);
+    }
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<Usuario> findByEmail(@PathVariable String email) throws Exception {
+        Usuario usuario = usuarioService.findByEmail(email);
+        return ResponseEntity.ok(usuario);
     }
 }
--- src/main/java/com/ecolimpio/ecommerce/services/UsuarioService.java
+++ src/main/java/com/ecolimpio/ecommerce/services/UsuarioService.java
@@ -1,13 +1,28 @@
 package com.ecolimpio.ecommerce.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ecolimpio.ecommerce.models.entities.Usuario;
 import com.ecolimpio.ecommerce.repositories.UsuarioRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class UsuarioService extends BaseService<Usuario, String> {
     public UsuarioService(UsuarioRepository usuarioRepository) {
         super(usuarioRepository);
+    }
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario findByEmail(String email) throws Exception {
+        try {
+            return usuarioRepository.findByEmail(email).orElse(null);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
